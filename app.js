1d35f052659bd32b1dcb5f613a71964917582137
SwitchTia <oblasser.tatiana@gmail.com>
2025-12-16 14:27:20 +0100

    Feat: json object with each city

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 //console.log("hey");
 import express from "express"
+import cities from "./cities.js";
 
 const app = express();
 const port = 3000;
 
+
+// Creiamo il progetto base con una rotta / 
+// che ritorna un testo semplice con scritto ”Server del mio blog”
 app.get("/", (req, res) => {
     res.send("Server of my blog")
 })
+
+
+
+// Creiamo poi una rotta /bacheca che restituisca 
+// un oggetto json con la lista dei post e il conteggio, partendo da un array.
+app.get("/cities", (req, res) => {
+
+    const newArray = cities.map((city) => {
+    return {
+      ...city, 
+      foto:`/cities/${city.id}.png`
+    }
+  })
+  const city = {
+    info: {
+      total: cities.length,
+    },
+    result: newArray
+  };
+  
+  res.json(city);
+
+})
+
+
+
+
 
 
 app.listen(port, function () {
