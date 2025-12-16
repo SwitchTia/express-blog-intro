6dc6cf17aac00453e8500d0ac4d472f9808256d3
SwitchTia <oblasser.tatiana@gmail.com>
2025-12-16 13:54:52 +0100

    Feat: File public img

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-console.log("hey");
+//console.log("hey");
 import express from "express"
 
 const app = express();
 const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Server of my blog")
+})
+
+
+app.listen(port, function () {
+  console.log("Server is connected on port " + port);
+});
